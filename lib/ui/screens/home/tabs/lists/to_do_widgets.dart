c95ff44e917c4c939e3c2fd5895b4eef5bf4cb8b
Mohamed-lalah <mohamedlalah918@gmail.com>
2023-12-04 19:26:26 +0200

    Importing the Slidable action and adding it to the task design

--- lib/ui/screens/home/tabs/lists/to_do_widgets.dart
+++ lib/ui/screens/home/tabs/lists/to_do_widgets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_assistant/ui/utilities/app_colors.dart';
 import 'package:productivity_assistant/ui/utilities/app_theme.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 class ToDoWidgets extends StatelessWidget {
   const ToDoWidgets({super.key});
 
@@ -10,43 +10,57 @@ class ToDoWidgets extends StatelessWidget {
     double height=  MediaQuery.of(context).size.height;
     double width=  MediaQuery.of(context).size.width;
 
-    return Padding(
+    return Container(
       padding:  EdgeInsets.all(height*0.02),
-      child: Container(
-        padding: EdgeInsets.all(width*0.04),
-        height: MediaQuery.of(context).size.height*0.15,
-        width: MediaQuery.of(context).size.width*0.85,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-          color: AppColors.white
+      child: Slidable(
+        startActionPane:  ActionPane(
+            motion: BehindMotion(),
+            children:[
+             SlidableAction(onPressed: (_){}
+                 ,backgroundColor: Colors.red,
+                 foregroundColor: Colors.white,
+                 icon: Icons.delete,
+               label: "delete ",
+               borderRadius: BorderRadius.circular(12),
+               )
+            ]
         ),
-        child:  Row(
-          children: [
-            Padding(
-              padding:  EdgeInsets.only(bottom: height*0.01,top: height*0.01),
-              child: VerticalDivider(),
-            ),
-             Padding(
-               padding:  EdgeInsets.all(width*0.02),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Play BasketBall",style: AppTheme.taskTitleTextStyle,),
-                  SizedBox(height: height*0.01,),
-                  Text("Date",style: AppTheme.taskDiscreptionTextStyle)
-                ],
-            ),
-             ),
-            Spacer(),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 18,vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AppColors.primiary,
-                ),
-                child: Icon(Icons.check,color: AppColors.white,size: 35,))
-          ],
+        child: Container(
+          padding: EdgeInsets.all(width*0.04),
+          height: MediaQuery.of(context).size.height*0.15,
+          width: MediaQuery.of(context).size.width*0.85,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: AppColors.white
+          ),
+          child:  Row(
+            children: [
+              Padding(
+                padding:  EdgeInsets.only(bottom: height*0.01,top: height*0.01),
+                child: VerticalDivider(),
+              ),
+               Padding(
+                 padding:  EdgeInsets.all(width*0.02),
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Play BasketBall",style: AppTheme.taskTitleTextStyle,),
+                    SizedBox(height: height*0.01,),
+                    Text("Date",style: AppTheme.taskDiscreptionTextStyle)
+                  ],
+              ),
+               ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 18,vertical: 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: AppColors.primiary,
+                  ),
+                  child: Icon(Icons.check,color: AppColors.white,size: 35,))
+            ],
+          ),
         ),
       ),
     );
